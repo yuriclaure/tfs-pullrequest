d7c52630ea6c345b91aa262e4884f120ba885fac
Yuri Claure <yuriclaure@gmail.com>
2016-05-13 16:51:30 -0400

    Criando validações de todos os comandos e organizando classes

--- main.py
+++ main.py
@@ -1,8 +1,51 @@
-import sys
-from git import Repo
-from commands import *
+import click
+import random
+from repository import Repository
 
-repo = Repo('.')
+pass_repository = click.make_pass_decorator(Repository, ensure=True)
+
+@click.group()
+@click.version_option('1.0', prog_name="Code review")
+def cr():
+	pass
+
+@cr.command(short_help="List, create, or finish a feature")
+@click.argument("feature_name", required=False)
+@click.option("--finish", "-f", is_flag=True, help="Finish feature")
+@pass_repository
+def feature(repository, feature_name, finish):
+	if finish:
+		repository.finish_feature(feature_name)
+	elif feature_name:
+		repository.create_feature(feature_name)
+	else:
+		repository.list_features()
+
+@cr.command(short_help="Move to another feature")
+@click.argument("feature_name", type=click.STRING)
+@pass_repository
+def move(repository, feature_name):
+	repository.move_to_feature(feature_name)
+
+@cr.command(short_help="Creates/updates a pull request for feature")
+@click.argument("feature_name", required=False)
+@click.option("--title", "-t", prompt="Title of pull request")
+@click.option('--hotfix', is_flag=True)
+@pass_repository
+def review(repository, feature_name, title, hotfix):
+	repository.review_feature(feature_name, title, hotfix)
+
+@cr.command(short_help="Push changes of a feature to server")
+@click.argument("feature_name", required=False)
+@pass_repository
+def share(repository, feature_name):
+	repository.share_feature(feature_name)
+
+@cr.command(short_help="Pull changes from master into feature")
+@click.argument("feature_name", required=False)
+@pass_repository
+def update(repository, feature_name):
+	repository.update_feature(feature_name)
 
 if __name__ == '__main__':
 	cr()
---
+++ repository.py
@@ -0,0 +1,58 @@
+from git import Repo
+from uuid import uuid4
+import click
+
+class Repository:
+
+	def create_feature(self, feature_name):
+		self.__assert_branch_does_not_exists(feature_name)
+		print("CREATE FEATURE " + str(feature_name))
+		#current_branch = repo.head
+		#nova_branch = repo.create_head(feature_name)
+		#current_branch.reset(commit="origin/master", working_tree=True)
+		
+		#TODO: salvar title da pull request pra usar quando for mandar pro servidor
+		#repo.head.reference = nova_branch
+
+	def list_features(self):
+		print("LIST")
+
+	def finish_feature(self, feature_name):
+		if not feature_name: 
+			feature_name = self.__current_branch_name()
+		print("FINISH FEATURE " + str(feature_name))
+
+	def move_to_feature(self, feature_name):
+		self.__assert_branch_exists(feature_name)
+		print("MOVING TO FEATURE " + str(feature_name))
+
+	def review_feature(self, feature_name, title, hotfix):
+		if not feature_name: 
+			feature_name = self.__current_branch_name()
+		print("REVIEW FEATURE " + str(title) + " ON BRANCH " + feature_name + (" AS HOTFIX" if hotfix else ""))
+
+	def share_feature(self, feature_name):
+		if not feature_name: 
+			feature_name = self.__current_branch_name()
+		print("SHARE FEATURE " + str(feature_name))
+
+	def update_feature(self, feature_name):
+		if not feature_name: 
+			feature_name = self.__current_branch_name()
+		print("UPDATE FEATURE " + str(feature_name))
+
+	def __assert_branch_does_not_exists(self, value):
+		if self.__branch_exists(value):
+			raise click.UsageError("Branch name " + value + " already assigned to another feature")
+		return value
+
+	def __assert_branch_exists(self, value):
+		if not self.__branch_exists(value):
+			raise click.UsageError("Couldn't find feature with branch name " + value)
+		return value
+
+	def __current_branch_name(self):
+		return "master"
+
+	def __branch_exists(self, feature_name):
+		return False
